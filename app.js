49689e7b33627225671dbae1fa4c3c36bf4c0fdc
Javone <javosmith@indigoempirellc.com>
2025-06-23 15:58:48 -0400

    added logic to update counter on DOM depending on input

--- app.js
+++ app.js
@@ -1,7 +1,3 @@
-let count = 0
-let countHolder = document.createElement('div')
-countHolder.textContent = count
-
 let body = document.querySelector('body')
 
 let container = document.createElement('div')
@@ -10,16 +6,21 @@ let h1Ctr = document.createElement('h1')
 h1Ctr.textContent = 'Counter'
 
 let inc = document.createElement('button')
-inc.setAttribute('id','inc')
+inc.setAttribute('class','btn')
 inc.textContent = 'increase'
 
 let reset = document.createElement('button')
-reset.setAttribute('id','reset')
+reset.setAttribute('class','btn')
 reset.textContent = 'reset'
 
 let dec = document.createElement('button')
-dec.setAttribute('id','dec')
+dec.setAttribute('class','btn')
 dec.textContent = 'decrease'
+
+let count = 0
+let countHolder = document.createElement('div')
+countHolder.setAttribute('id','count')
+countHolder.textContent = count
 
 body.append(container)
 container.appendChild(h1Ctr)
@@ -29,6 +30,30 @@ container.appendChild(reset)
 container.appendChild(dec)
 
 // -------- CODE HERE
+let btns = document.querySelectorAll('.btn')
+btns.forEach(btn => {btn.addEventListener('click', (e) => update(e))})
+
+const update = (e) => {{
+    switch (e.target.textContent) {
+        case 'increase':
+            count++
+            countHolder.textContent = count
+            break;
+        case 'decrease':
+            count--
+            countHolder.textContent = count
+            break;
+        default:
+            count = 0
+            countHolder.textContent = count
+            break;
+    }
+}}
+
+
+
+
+
 
 
 
